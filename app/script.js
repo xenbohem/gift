3030fd5ac8113ec4292e7d9e04628e4840a53956
xenbohem <xenv@mailfence.com>
2025-12-25 03:02:35 -0800

    Update script.js

--- app/script.js
+++ app/script.js
@@ -1,13 +1,12 @@
 // Save day
 function openDay(dayId) {
   localStorage.setItem("lastDay", dayId);
-  // open devotional logic here
 }
 
-// Restore on load
 window.addEventListener("load", () => {
   const last = localStorage.getItem("lastDay");
-  if (last) {
-    openDay(last);
-  }
+  if (last) openDay(last);
 });
+
+
+
